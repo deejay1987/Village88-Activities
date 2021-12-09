13cbc3ccd647fbd182f2ba0938088416dc2cde7c
Denverjack Lagasca <83049116+deejay1987@users.noreply.github.com>
2021-12-09 11:46:34 +0800

    update

--- Pet Shelter/assets/js/custom/app.js
+++ Pet Shelter/assets/js/custom/app.js
@@ -1,15 +1,114 @@
 $(document).ready(function(){
+    var count = 0;
 
-    
     $("body")
-        .on("click", "#submit", function(){
-        let addForm = $("#addForm");
+    .on("submit", "#addForm", function(){
+        valid = true;
         
-            console.log('submit');
-            console.log($(addForm).serializeArray());
+        if($("#petname").val() == "" || $("#petname").val().length < 3){
+            $("#petname").css('border','1px solid red');
+            valid = false;
+            return false;
+        }
+        else{
+            $("#petname").css('border','none');
+        }
+
+        if($("#pettype").val() == "" || $("#pettype").val().length < 3){
+            $("#pettype").css('border','1px solid red');
+            valid = false;
+            return false;
+        }
+        else{
+            $("#pettype").css('border','none');
+        }
+
+        if($("#description").val() == "" || $("#description").val().length < 3){
+            $("#description").css('border','1px solid red');
+            valid = false;
+            return false;
+        }
+        else{
+            $("#description").css('border','none');
+        }
+
+        if($("#skills").val() == "" || $("#skills").val().length < 3){
+            $("#skills").css('border','1px solid red');
+            valid = false;
+            return false;
+        }
+        else{
+            $("#skills").css('border','none');
+        }
+    })
+    .on("click", "#submit", function(){
+        // array
+        let add_form_data_array = $("#addForm").serializeArray();
+        let table_td = "";
+
+        for (i=0; i<add_form_data_array.length; i++) {
+            
+            if(add_form_data_array[i].name == "Description" || add_form_data_array[i].name  == "Skills"){
+               
+            }
+            else{
+                table_td += "<td>" + add_form_data_array[i].value + "</td>";
+            }
+        }
+        $("#pets").append(
+            "<tr>" + table_td + `
+                <td>
+                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#details">
+                        <img src="../assets/images/details.png" alt="Vector">
+                        Details
+                    </button>
+                </td>
+                <td>
+                    <button class="btn" data-bs-toggle="modal" data-bs-target="#edit">
+                        <img src="../assets/images/edit.png" alt="Vector">
+                        Edit
+                    </button>
+                </td>
+            </tr>`);
+    })
+
+    .on("click", "submit", function(){
+        let pet = "";
+        var petName = $("#petname").val(); /*passing data to the details and edit header modal*/
+        $("#details_about").html("Details about: " + petName);
+        $("#edit_details").html("Edit details: " + petName);
+
+        if(add_form_data_array[i].name == "petname"){
+
+        }
+        else{
+            pet += "<td>" + add_form_data_array[i].value + "</td>"
+        }
+        $("#table_modal").html("<tr>" + pet + "</tr>");  
+    })
+    
+    .on("click", "#like_pet", function(){
+        count++;
         
-        
-        /*if($(addForm).empty()){//if input value is empty and less 3 characters
+        if(count === 1) {
+            $("#likes").html("1 Like");
+        }
+        else {
+            $("#likes").html(count + " Likes");
+        }
+    });
+});
+
+
+    // Pet Details Modal
+    // pet name will appear to Modal Title ".modal-title"
+    // pet type, description and skills will appear to the modal body ".modal-body"
+
+   
+
+   
+// });
+        /*if(){//if input value is empty and less 3 characters
             // add border red in input
         }
         else{
@@ -17,9 +116,6 @@ $(document).ready(function(){
             // remove the border red in input.
         }
 
-        
-
-        
         // SUBMITv c
         if(){ //if there is any red border in the form
             // Do no submit the form;
@@ -28,6 +124,6 @@ $(document).ready(function(){
             // Submit the form. and append to table
             // .append()
         }*/
-     });
+//      });
   
-});
+// });
