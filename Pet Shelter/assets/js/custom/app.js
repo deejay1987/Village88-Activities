054b428df59d8f4919baf8f5e44f24039ea902c9
Denverjack Lagasca <83049116+deejay1987@users.noreply.github.com>
2021-12-10 13:25:17 +0800

    update

--- Pet Shelter/assets/js/custom/app.js
+++ Pet Shelter/assets/js/custom/app.js
@@ -1,57 +1,73 @@
 $(document).ready(function(){
-    var count = 0;
+    // var count = 0;
 
     $("body")
     .on("submit", "#addForm", function(e){
         e.preventDefault();
         let addForm = $(this);
-        let add_form_data_array = addForm.serializeArray();
-        let table_td = "";
+        let add_form_data_array = addForm.find("input");
+        // let table_td = "";
 
-        if($("#petname").val() == "" || $("#petname").val().length < 3 ? $("#petname").addClass("border_red") : $("#petname").removeClass("border_red"));
+        if(add_form_data_array.val() == "" || (add_form_data_array).val().length < 3 ? (add_form_data_array).addClass("border_red") : (add_form_data_array).removeClass("border_red"));
+        if(!add_form_data_array.hasClass("border_red")){
+            $("#pets").append(
+                "<tr>" + add_form_data_array + `
+                    <td>
+                        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#details">
+                            <img src="../assets/images/details.png" alt="Vector">
+                            Details
+                        </button>
+                    </td>
+                    <td>
+                        <button class="btn" data-bs-toggle="modal" data-bs-target="#edit">
+                            <img src="../assets/images/edit.png" alt="Vector">
+                            Edit
+                        </button>
+                    </td>
+                </tr>`);
+        }
+        // if($("#pettype").val() == "" || $("#pettype").val().length < 3 ? $("#pettype").addClass("border_red") : $("#pettype").removeClass("border_red"));
 
-        if($("#pettype").val() == "" || $("#pettype").val().length < 3 ? $("#pettype").addClass("border_red") : $("#pettype").removeClass("border_red"));
+        // if($("#description").val() == "" || $("#description").val().length < 3 ? $("#description").addClass("border_red") : $("#description").removeClass("border_red"));
 
-        if($("#description").val() == "" || $("#description").val().length < 3 ? $("#description").addClass("border_red") : $("#description").removeClass("border_red"));
-
-        if($("#skills").val() == "" || $("#skills").val().length < 3 ? $("#skills").addClass("border_red") : $("#skills").removeClass("border_red"));
+        // if($("#skills").val() == "" || $("#skills").val().length < 3 ? $("#skills").addClass("border_red") : $("#skills").removeClass("border_red"));
            
 
-        for (i=0; i<add_form_data_array.length; i++) {
+        // for (i=0; i<add_form_data_array.length; i++) {
             
-            if(add_form_data_array[i].name == "description" || add_form_data_array[i].name  == "skills"){
+        //     if(add_form_data_array[i].name == "description" || add_form_data_array[i].name  == "skills"){
                
-            }
-            else{
-                table_td += "<td>" + add_form_data_array[i].value + "</td>";
-            }
-        }
-        $("#pets").append(
-            "<tr>" + table_td + `
-                <td>
-                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#details">
-                        <img src="../assets/images/details.png" alt="Vector">
-                        Details
-                    </button>
-                </td>
-                <td>
-                    <button class="btn" data-bs-toggle="modal" data-bs-target="#edit">
-                        <img src="../assets/images/edit.png" alt="Vector">
-                        Edit
-                    </button>
-                </td>
-            </tr>`);
+        //     }
+        //     else{
+        //         table_td += "<td>" + add_form_data_array[i].value + "</td>";
+        //     }
+        // }
+        // $("#pets").append(
+        //     "<tr>" + table_td + `
+        //         <td>
+        //             <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#details">
+        //                 <img src="../assets/images/details.png" alt="Vector">
+        //                 Details
+        //             </button>
+        //         </td>
+        //         <td>
+        //             <button class="btn" data-bs-toggle="modal" data-bs-target="#edit">
+        //                 <img src="../assets/images/edit.png" alt="Vector">
+        //                 Edit
+        //             </button>
+        //         </td>
+        //     </tr>`);
     })
-    .on("click", "#like_pet", function(){
-        count++;
+    // .on("click", "#like_pet", function(){
+    //     count++;
         
-        if(count === 1) {
-            $("#likes").html("1 Like");
-        }
-        else {
-            $("#likes").html(count + " Likes");
-        }
-    })
+    //     if(count === 1) {
+    //         $("#likes").html("1 Like");
+    //     }
+    //     else {
+    //         $("#likes").html(count + " Likes");
+    //     }
+    // })
 
     // .on("click", "submit", function(){
     //     let pet = "";
