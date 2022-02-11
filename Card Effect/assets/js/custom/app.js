00edea858c5738b4ab0392b5d3b403fe238ffc04
Denverjack Lagasca <83049116+deejay1987@users.noreply.github.com>
2022-02-11 17:34:36 +0800

    Upload Card Effect folder

---
+++ Card Effect/assets/js/custom/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    let container_cards = $(".container_cards");
+    let current_card = container_cards.filter(".active");
+
+
+    $("body")
+        .on("click", "#next", function(){
+            let next_card = current_card.next();
+
+            current_card.removeClass("active");
+
+            if(next_card.length){
+                current_card = next_card.addClass("active");
+            }
+            else{
+                current_card =container_cards.first().addClass("active");
+            }
+            
+        })
+
+        .on("click", "#previous", function(){
+            let previous_card = current_card.prev();
+           
+            current_card.removeClass('active');
+
+            if (previous_card.length) {
+                current_card = previous_card.addClass('active');
+            } else {
+                current_card = container_cards.last().addClass('active');
+            }
+        })
+});
